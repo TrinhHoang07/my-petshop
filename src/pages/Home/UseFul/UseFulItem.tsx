206fc015a882e3080cc3105f0f434532f386b2c0
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-05-27 07:54:05 +0700

    update css defaulyLayout, responsive LayoutDetailNew

--- src/pages/Home/UseFul/UseFulItem.tsx
+++ src/pages/Home/UseFul/UseFulItem.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './UseFul.module.scss';
 import classNames from 'classnames/bind';
 
