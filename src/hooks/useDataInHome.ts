4a59369e145e6d3d156b81dc532bd92ad8261668
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-08-06 21:50:26 +0700

    add hooks useDataInHome page

---
+++ src/hooks/useDataInHome.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+export type _T_DataItemHome = {
+    id: number;
+    name: string;
+    previewUrl: string;
+    price: number;
+};
+
+function useDataInHome(url: string) {
+    const [data, setData] = useState<_T_DataItemHome[]>([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.data.length > 0) {
+                    const items: _T_DataItemHome[] = data.data.map((item: any) => ({
+                        id: item.id,
+                        name: item.name,
+                        price: item.price,
+                        previewUrl: item.preview_url,
+                    }));
+
+                    setData(items);
+                }
+            })
+            .catch((err) => console.error(err));
+    }, []);
+
+    return data;
+}
+
+export default useDataInHome;
