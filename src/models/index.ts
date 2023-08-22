11df6bb2b2a823e2c01fb37e87b2db565d604b7c
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-08-22 23:36:13 +0700

    create models (Products)

---
+++ src/models/index.ts
@@ -0,0 +1,12 @@
+export type T_Products = {
+    id: number;
+    name: string;
+    description: string;
+    sub_description: string;
+    type: string;
+    price: number;
+    quantity: number;
+    color?: string;
+    rate: number;
+    preview_url: string;
+};
