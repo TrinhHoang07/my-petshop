d28e92c17ce639b9cf55d887854dd2c089a25efe
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-08-24 15:28:37 +0700

    handle logic search in Search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
 import { useLocation } from 'react-router-dom';
 import { BiSearch } from 'react-icons/bi';
+import { useDebounce } from '../../hooks';
+import { AiOutlineLoading } from 'react-icons/ai';
+import { CardItemFlip } from '../../components/CardItemFlip';
 
 const cx = classNames.bind(styles);
 
@@ -13,6 +16,35 @@ type TStateSearch = {
 function Search() {
     const { state }: { state: TStateSearch } = useLocation();
     const [searchText, setSearchText] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>('Không có tìm kiếm gần đây');
+    const [data, setData] = useState([]);
+    const debounced = useDebounce(searchText, 750);
+
+    useEffect(() => {
+        if (debounced.trim().length > 0) {
+            setLoading(true);
+            fetch(`http://localhost:3009/products/search?search=${debounced}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setLoading(false);
+
+                    if (data.data.length > 0) {
+                        setData(data.data);
+                    } else {
+                        setData([]);
+                        setMessage('Không có kết quả tìm kiếm !');
+                    }
+                })
+                .catch((err) => console.log('err: ', err));
+        }
+    }, [debounced]);
+
+    useEffect(() => {
+        if (state && state.q && state.q.length > 0) {
+            setSearchText(state.q);
+        }
+    }, [state]);
 
     return (
         <div className={cx('wrapper')}>
@@ -26,31 +58,26 @@ function Search() {
                             type="text"
                             placeholder="Nhập từ khóa để tìm kiếm..."
                         />
-                        <span>
-                            <BiSearch />
-                        </span>
-                        {!!searchText && (
-                            <div className={cx('search-preview')}>
-                                <h6>Kết quả tìm kiếm</h6>
-                                <div className={cx('result-search-preview')}>
-                                    <p>1</p>
-                                    <p>1</p>
-                                    <p>1</p>
-                                    <p>1</p>
-                                </div>
-                            </div>
-                        )}
+                        <span>{!loading ? <BiSearch /> : <AiOutlineLoading className={cx('loading-input')} />}</span>
                     </div>
                 </div>
                 <div className={cx('contents')}>
-                    <h2>
-                        Kết quả tìm kiếm cho '<span>{state ? state.q : searchText}</span>'
-                    </h2>
+                    {data.length > 0 && <h2>Kết quả tìm kiếm</h2>}
                     <div className={cx('containers')}>
-                        <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                        <div>4</div>
+                        {data.length > 0 ? (
+                            data.map((item: any) => (
+                                <CardItemFlip
+                                    key={item.id}
+                                    to={`product/cat/${item.id}`}
+                                    title="MÈO CẢNH"
+                                    name={item.name}
+                                    src={item.preview_url}
+                                    price={item.price.toString()}
+                                />
+                            ))
+                        ) : (
+                            <p style={{ textAlign: 'center' }}>{message}</p>
+                        )}
                     </div>
                 </div>
             </div>
