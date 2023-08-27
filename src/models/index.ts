6b2c6004eb23bf8bfb8dfe1eb4ec00b994d07b39
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-08-27 16:50:45 +0700

    add model typescript (Cart, T_Category, T_Detail, Customer, T_Login, T_Search, T_Shop, T_AddCart)

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,4 @@
-export type T_Products = {
+export type T_Product = {
     id: number;
     name: string;
     description: string;
@@ -9,4 +9,82 @@ export type T_Products = {
     color?: string;
     rate: number;
     preview_url: string;
+};
+
+// categories page
+export type T_Cart = {
+    carts_id: number;
+    carts_product_id: number;
+    carts_customer_id: number;
+    carts_quantity: number;
+    customer_address: string;
+    customer_avatar_path: string;
+    customer_birth_date: string;
+    customer_gender: string;
+    customer_name: string;
+    customer_phone_number: string;
+    product_color: string;
+    product_description: string;
+    product_name: string;
+    product_preview_url: string;
+    product_price: number;
+    product_rate: number;
+    product_type: string;
+    carts_created_at: string;
+};
+
+export type T_Categorys = {
+    message: string;
+    statusCode: number;
+    data: T_Cart[];
+};
+
+// type detail page
+export type T_Detail = {
+    message: string;
+    statusCode: number;
+    data: T_Product;
+};
+
+// type login page
+export type Customer = {
+    id: number;
+    name: string;
+    email: string;
+    gender: string;
+    avatar: string;
+    address: string;
+    phone_number: string;
+    birth_day: string;
+    access_token: string;
+};
+export type T_Login = {
+    message: string;
+    statusCode: number;
+    data: Customer;
+};
+
+// type search page
+export type T_Search = {
+    message: string;
+    statusCode: number;
+    data: T_Product[];
+};
+
+// type shop page
+export type T_Shop = {
+    message: string;
+    statusCode: number;
+    data: T_Product[];
+};
+
+// type add to cart
+export type T_AddCart = {
+    message: string;
+    statusCode: number;
+    data: {
+        customer_id: number;
+        product_id: number;
+        quantity: number;
+    };
 };
