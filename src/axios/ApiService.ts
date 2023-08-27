8e0d4b6536c0f015421e207144605d133d709b90
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2023-08-27 23:31:05 +0700

    create axios instance & apiService

---
+++ src/axios/ApiService.ts
@@ -0,0 +1,59 @@
+// UPDATE TYPESCRIPT LATER
+
+import { AxiosClientApi } from './AxiosInstance';
+
+export class ApiService {
+    get auth() {
+        const route = {
+            register: 'register',
+            login: 'login',
+        };
+
+        return {
+            register: (data: any): Promise<any> => AxiosClientApi.post(route.register, data),
+            login: (data: any): Promise<any> => AxiosClientApi.post(route.login, data),
+        };
+    }
+
+    get products() {
+        const route = {
+            getProducts: 'products/all',
+            getProduct: (id: string) => `products/product/${id}`,
+            searchProducts: 'products/search',
+            randomProducts: 'products/random',
+        };
+
+        return {
+            getProducts: (query?: any): Promise<any> => AxiosClientApi.get(route.getProducts, query),
+            getProduct: (id: string, query?: any): Promise<any> => AxiosClientApi.get(route.getProduct(id), query),
+            searchProducts: (query?: any): Promise<any> => AxiosClientApi.get(route.searchProducts, query),
+            randomProducts: (): Promise<any> => AxiosClientApi.get(route.randomProducts),
+
+            route: route,
+        };
+    }
+
+    get carts() {
+        const route = {
+            getCartsByUserId: (userId: string) => `carts/cart-by-customer/${userId}`,
+            addToCart: 'carts/add-to-cart',
+        };
+
+        return {
+            getCartsByUserId: (userId: string, token: string) =>
+                AxiosClientApi.get(route.getCartsByUserId(userId), null, {
+                    headers: {
+                        Authorization: 'Bearer ' + token,
+                    },
+                }),
+            addToCart: (data: any, token: string) =>
+                AxiosClientApi.post(route.addToCart, data, {
+                    headers: {
+                        Authorization: 'Bearer ' + token,
+                    },
+                }),
+
+            route,
+        };
+    }
+}
