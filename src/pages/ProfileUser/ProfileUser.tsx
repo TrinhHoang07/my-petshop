fd599c0520da717f4b5272e9740b9de96a1ba7a0
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-01-02 11:36:20 +0700

    handle accpet, cancel, add new friend in ProfileUser

--- src/pages/ProfileUser/ProfileUser.tsx
+++ src/pages/ProfileUser/ProfileUser.tsx
@@ -1,20 +1,25 @@
 import classNames from 'classnames/bind';
 import styles from './ProfileUser.module.scss';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { dataProfileUser } from '../../store';
 import { TProfileUser } from '../../models';
 import { Button } from '../../components/Button';
 import { useEffect, useState } from 'react';
 import { ApiService } from '../../axios/ApiService';
 import { useSessionContext } from '../../context/SessionContext';
+import { confirmDialog } from 'primereact/confirmdialog';
+import { useConfirmToast } from '../../context/ConfirmAndToastContext';
+import { useSocketContext } from '../../context/SocketContext';
 
 const cx = classNames.bind(styles);
 
 function ProfileUser() {
-    const data = useRecoilValue<TProfileUser>(dataProfileUser);
+    const [data, setData] = useRecoilState<TProfileUser>(dataProfileUser);
+    const message = useConfirmToast();
     const [idsInvited, setIdsInvited] = useState<number[]>([]);
     const [idsGiveInvite, setIdsGiveInvite] = useState<number[]>([]);
     const apiService = new ApiService();
+    const socket = useSocketContext();
     const [values] = useSessionContext();
 
     useEffect(() => {
@@ -43,6 +48,104 @@ function ProfileUser() {
             });
     };
 
+    const handleAddNewInviteFriend = () => {
+        apiService.friendship
+            .addNewInviteFriend(
+                {
+                    status: 'waiting',
+                    customer_invite: values.user?.id,
+                    customer_id: data.id,
+                },
+                values.user?.token ?? '',
+            )
+            .then((res) => {
+                if (res.message === 'success') {
+                    handleGetIdsInvited();
+
+                    message?.toast?.current?.show({
+                        severity: 'success',
+                        summary: 'Thành công',
+                        detail: 'Gửi yêu cầu thành công!',
+                        life: 2000,
+                    });
+                } else {
+                    message?.toast?.current?.show({
+                        severity: 'error',
+                        summary: 'Thất bại',
+                        detail: 'Có lỗi, vui lòng thử lại!',
+                        life: 2500,
+                    });
+                }
+            })
+            .catch((err) => console.error(err));
+    };
+
+    const handleRemoveInvite = () => {
+        confirmDialog({
+            message: 'Bạn có chắc chắn muốn hủy yêu cầu không?',
+            position: 'top',
+            header: 'Hủy yêu cầu',
+            acceptLabel: 'Đồng ý',
+            rejectLabel: 'Hủy bỏ',
+            icon: 'pi pi-exclamation-triangle',
+            accept() {
+                apiService.friendship
+                    .deleteFriendshipById(
+                        {
+                            customer_invite: values.user?.id,
+                            customer_id: data.id,
+                        },
+                        values.user?.token ?? '',
+                    )
+                    .then((res) => {
+                        if (res.message === 'success') {
+                            handleGetIdsInvited();
+                            message?.toast?.current?.show({
+                                severity: 'success',
+                                summary: 'Thành công',
+                                detail: 'Hủy yêu cầu thành công!',
+                                life: 2000,
+                            });
+                        } else {
+                            message?.toast?.current?.show({
+                                severity: 'error',
+                                summary: 'Thất bại',
+                                detail: 'Có lỗi, vui lòng thử lại!',
+                                life: 2500,
+                            });
+                        }
+                    })
+                    .catch((err) => console.error(err));
+            },
+        });
+    };
+
+    const handleAcceptInvite = () => {
+        apiService.friendship
+            .acceptFriendship(
+                {
+                    customer_invite: data.id,
+                    customer_id: values.user?.id,
+                    status: 'friended',
+                },
+                values.user?.token ?? '',
+            )
+            .then((res: any) => {
+                if (res.message === 'success') {
+                    setData((prev: TProfileUser) => ({
+                        ...prev,
+                        isFriend: true,
+                    }));
+
+                    socket.current?.emit('accept-friend', {
+                        id: data.id,
+                        status: 'success',
+                    });
+                }
+            })
+            .catch((err) => console.error(err));
+    };
+
     return (
         <div className={cx('profile-user')}>
             <div className={cx('profile-user-wrapper')}>
@@ -61,11 +164,11 @@ function ProfileUser() {
                                 <Button>Nhắn tin</Button>
                             </>
                         ) : idsInvited.includes(data.id) ? (
-                            <Button>Hủy yêu cầu</Button>
+                            <Button onClick={handleRemoveInvite}>Hủy yêu cầu</Button>
                         ) : idsGiveInvite.includes(data.id) ? (
-                            <Button>Xác nhận</Button>
+                            <Button onClick={handleAcceptInvite}>Xác nhận</Button>
                         ) : (
-                            <Button>Thêm bạn bè</Button>
+                            <Button onClick={handleAddNewInviteFriend}>Thêm bạn bè</Button>
                         )}
                     </div>
                     <p className={cx('description')}>
