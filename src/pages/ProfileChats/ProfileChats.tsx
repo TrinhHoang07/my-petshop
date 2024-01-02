17651c61281412b54e4ccae2b8e9b9376e83adc8
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-01-02 22:09:15 +0700

    build UI Profile Chat #2

--- src/pages/ProfileChats/ProfileChats.tsx
+++ src/pages/ProfileChats/ProfileChats.tsx
@@ -3,6 +3,7 @@ import styles from './Profile.module.scss';
 import img from '../../assets/images/beyeu.jpg';
 import { LayoutProfile } from '../../components/Layout/LayoutProfile';
 import { IoSend } from 'react-icons/io5';
+import { MdOutlineSearch } from 'react-icons/md';
 
 const cx = classNames.bind(styles);
 
@@ -11,7 +12,96 @@ function Profile() {
         <LayoutProfile>
             <div className={cx('profile-chats')}>
                 <div className={cx('bar-chats')}>
-                    <div className={cx('item-chat')}></div>
+                    <div className={cx('container-bar')}>
+                        <h5>Đoạn chat</h5>
+                        <div className={cx('search-user')}>
+                            <MdOutlineSearch size={'2rem'} />
+                            <input type="text" placeholder="Tìm kiếm trong đoạn chat" />
+                        </div>
+                        <div className={cx('list-user')}>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                            <div className={cx('item-chat')}>
+                                <div className={cx('item-avatar')}>
+                                    <img src={img} alt="item-avatar" />
+                                </div>
+                                <div className={cx('item-info')}>
+                                    <h6>Van Hoang</h6>
+                                    <p className={cx('last-message')}>Bạn: hehe hehehe</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className={cx('chats-messages')}>
                     <div className={cx('head-chat')}>
